87609e3b7cea4504cd18241f20eea1553dffe70f
juan <juanayala@gmail.com>
2022-11-17 23:13:36 -0300

    add displayBoard function

--- src/public/script.js
+++ src/public/script.js
@@ -12,7 +12,9 @@ const mainContainer = document.getElementById('main-container');
 const boardSize = document.getElementById("board-size"); 
 /**
  * Getting the button element from the DOM to submit the size of the board.
+ * @constant
  * @type HTMLElement|null
+ * @default
  */
 const submitBoardSize = document.getElementById("submit-board-size")
 /**
@@ -20,9 +22,28 @@ const submitBoardSize = document.getElementById("submit-board-size")
  * @type number|String[]
  */
 const board = []
-console.log(submitBoardSize)
-
+/**
+ * This function displays the board with the amount of rows and columns as passed on the parameters.
+ * @param {number} size This is the amount of rows and columns that will be displayed.
+ */
+const displayBoard = (size)=>{
+    let HTMLSquare = ""
+    for(let i = 0; i < size; i++){
+        HTMLSquare += "<div class='square-container' >"
+        for(let j = 0; j < size; j++){
+            HTMLSquare +=  
+            `<div class="square">
+                <p>0</p>
+            </div>`
+        }
+        HTMLSquare += "</div> <br>"
+    }
+    //@ts-ignore
+    mainContainer.innerHTML = HTMLSquare
+}
 // @ts-ignore
 submitBoardSize.addEventListener("click", (e)=>{
     e.preventDefault()
+    // @ts-ignore
+    displayBoard(boardSize.value)
 })
